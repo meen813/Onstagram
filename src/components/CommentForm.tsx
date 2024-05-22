6874fd617ffde39e2ddf1dfc37168c3a29596c31
Ian Hwang <meen813@hotmail.com>
2024-05-21 23:44:28 -0700

    fix: Render user avatar image as circle on comment form
    - Adjust styling to ensure user avatar image is displayed as a circle shape.
    - Fix issue where the image was rendered as oval instead of a circle.

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -4,8 +4,8 @@ import Avatar from "./Avatar";
 
 
 type Props = {
-        authorUsername: string;
-        onPostComment: (comment: string) => void;
+    authorUsername: string;
+    onPostComment: (comment: string) => void;
 };
 
 export default function CommentForm({ authorUsername, onPostComment }: Props) {
@@ -13,26 +13,28 @@ export default function CommentForm({ authorUsername, onPostComment }: Props) {
     const buttonDisabled = comment.length === 0;
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
-            onPostComment(comment);
-            setComment('');
+        onPostComment(comment);
+        setComment('');
     };
     const { data: session } = useSession();
     const user = session?.user;
-    
-    return(
+
+    return (
         <form onSubmit={handleSubmit} className="flex items-center px-3 border-t border-neutral-300">
-        <Avatar image={user?.image} size="small" />
-        <input
-            className="w-full ml-2 border-none outline-none p-3"
-            type="text"
-            placeholder={`Add a comment for ${authorUsername}`}
-            required
-            value={comment}
-            onChange={e => setComment(e.target.value)}
-        />
-        <button disabled={buttonDisabled} className={`font-bold ${buttonDisabled? ' text-blue-200' : ' text-blue-500'}`}>
-            Post
-        </button>
-    </form>
+            <div>
+                <Avatar image={user?.image} size="small" highlight />
+            </div>
+            <input
+                className="w-full ml-2 border-none outline-none p-3"
+                type="text"
+                placeholder={`Add a comment for ${authorUsername}`}
+                required
+                value={comment}
+                onChange={e => setComment(e.target.value)}
+            />
+            <button disabled={buttonDisabled} className={`font-bold ${buttonDisabled ? ' text-blue-200' : ' text-blue-500'}`}>
+                Post
+            </button>
+        </form>
     )
 }
